29e73ea1e23b772e036e713179e5ec4f02690a77
RavenousReaper117 <m.o.lefebvre@hotmail.com>
2025-03-29 23:35:16 -0400

    dd

--- semaine09/js/scripts.js
+++ semaine09/js/scripts.js
@@ -2,13 +2,3 @@
 
 const h1 = document.querySelector("h1");
 
-// Sélectionne le h1 du header et sépare ses lettres
-let split = new SplitText("header h1", { type: "chars" });
-
-// Ajoute une classe spéciale aux dernières lettres de chaque mot
-split.chars.forEach((char, index, array) => {
-    let nextChar = array[index + 1];
-    if (!nextChar || nextChar.textContent === " ") {
-        char.classList.add("last-letter");
-    }
-})
